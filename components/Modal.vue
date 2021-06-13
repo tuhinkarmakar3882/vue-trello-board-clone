b2599c2db832ed0810ff38c67485e583957f90b0
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-06-13 20:48:52 +0530

    Added Modal.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ components/Modal.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="modal-component">
+    <header>
+      <slot name="header">Modal Title</slot>
+      <i v-if="hideCloseButton" class="mdi mdi-close mdi-24px" />
+    </header>
+
+    <main>
+      <slot name="body"></slot>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  props: {
+    hideCloseButton: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.modal-component {
+  display: grid;
+  grid-template-rows: auto 1fr;
+  width: clamp(300px, 100vw, 500px);
+  background: var(--body-background);
+  border-radius: var(--spacing-micro);
+  box-shadow: 0 0 4px hsla(0, 0%, 0%, 0.3), 0 0 0 100vh hsla(0, 0%, 0%, 0.7);
+  z-index: 1;
+
+  header {
+    border-radius: var(--spacing-micro) var(--spacing-micro) 0 0;
+    padding: 0 var(--spacing-standard);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: var(--primary-color);
+    * {
+      color: white;
+    }
+  }
+
+  main {
+    padding: var(--spacing-standard);
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,51 @@
 <template>
   <div class="index-page">
-    <i class="mdi mdi-trello mdi-48px" />
-    <h1>Nuxt.js Todo Board!</h1>
-    <p>This is my Super Duper Page!</p>
-    <small>And I Love it!</small>
-    <section>
-      <button class="primary-button">Click Me</button>
-      <button class="secondary-button">Click Me</button>
-      <button class="success-button">Click Me</button>
-      <button class="warning-button">Click Me</button>
-      <button class="danger-button">Click Me</button>
-    </section>
-    <a href="">Follow ME</a>
+    <Modal>
+      <template #header>
+        <h2>Get Started</h2>
+      </template>
+
+      <template #body>
+        <section class="modal-body">
+          <p>Please enter the initial number of columns to continue</p>
+          <input v-model="columnSize" type="number" />
+          <button
+            :disabled="!columnSize"
+            :class="columnSize && 'primary-button'"
+          >
+            Proceed
+          </button>
+        </section>
+      </template>
+    </Modal>
   </div>
 </template>
 
 <script>
-export default {}
+import Modal from '~/components/Modal'
+
+export default {
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      columnSize: null,
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped>
 .index-page {
   display: grid;
   place-items: center;
+
+  .modal-body {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 }
 </style>
